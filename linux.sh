22b8bfcfdc0a14a7de3517710ba293efd29e42f6
ajaaugustina <ajaaugustina54@gmail.com>
2023-11-25 15:35:15 -0500

    edited a command in linux.sh to return to my IP addresses

---
+++ linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "please enter your name:"
+read name
+echo " Reading... please wait"
+sleep 3
+echo "-----------------------------"
+echo "Your username is:" &&Whoami
+echo "-----------------------------"
+echo "This is one of my first linux scripts"
+sleep 2
+echo "Please wait while we process the date....."
+sleep 3
+echo "Made on:" && date
+echo "-----------------------------"
+sleep 2
+echo "My IP address is:"
+echo "-----------------------------"
+sleep 2
+curl ifconfig.me
+
+This is day 1 - project test
